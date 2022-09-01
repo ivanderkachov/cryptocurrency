173ad82181cdd1607125d9e3f256d633603df2ab
Деркачев Иван <derkachov_ivan@host-192-168-0-103.sohonet.ua>
2022-09-01 21:41:55 +0300

    add API for coins data

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
+const axios = require('axios')
 require("dotenv").config({ path: ".env" });
 const app = express()
 const router = express.Router()
@@ -33,6 +34,94 @@ if (process.env.NODE_ENV === "production") {
 app.get("/api/v1/info", (req, res) => {
   res.json({ status: "ok" });
 });
+
+app.get("/api/v1/coins", async (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://coinranking1.p.rapidapi.com/coins",
+    params: {
+      referenceCurrencyUuid: "yhjMzLPhuIDl",
+      timePeriod: "24h",
+      "tiers[0]": "1",
+      orderBy: "marketCap",
+      orderDirection: "desc",
+      limit: "50",
+      offset: "0",
+    },
+    headers: {
+      "X-RapidAPI-Key": "740d6c8fcamsh537b1e793f27e8ep18db1ejsnbf58664a5522",
+      "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+    },
+  };
+  const crypto = await axios(options).then(({ data }) => {
+    return data
+  })
+  res.json({ status: "ok", crypto });
+});
+
+app.get("/api/v1/:coinId/:timePeriod", async (req, res) => {
+  const { coinId, timePeriod} = req.params
+  const options = {
+    method: "GET",
+    url: `https://coinranking1.p.rapidapi.com/coin/${coinId}`,
+    params: { referenceCurrencyUuid: "yhjMzLPhuIDl", timePeriod: `${timePeriod}` },
+    headers: {
+      "X-RapidAPI-Key": "740d6c8fcamsh537b1e793f27e8ep18db1ejsnbf58664a5522",
+      "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+    },
+  };
+  const coinData = await axios(options).then(({ data }) => {
+    return data;
+  });
+  res.json({ status: "ok", coinData });
+});
+
+app.get("/api/v1/history/:coinId/:timePeriod", async (req, res) => {
+  const { coinId, timePeriod } = req.params;
+  const options = {
+    method: "GET",
+    url: `https://coinranking1.p.rapidapi.com/coin/${coinId}/history`,
+    params: {
+      referenceCurrencyUuid: "yhjMzLPhuIDl",
+      timePeriod: `${timePeriod}`,
+    },
+    headers: {
+      "X-RapidAPI-Key": "740d6c8fcamsh537b1e793f27e8ep18db1ejsnbf58664a5522",
+      "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+    },
+  };
+  const coinData = await axios(options).then(({ data }) => {
+    return data;
+  });
+  res.json({ status: "ok", coinData });
+});
+
+app.get("/api/v1/news/:coin", async (req, res) => {
+  const { coin } = req.params
+  const options = {
+    method: "GET",
+    url: "https://bing-news-search1.p.rapidapi.com/news/search",
+    params: {
+      q: `${coin}`,
+      setLang: "EN",
+      freshness: "Day",
+      textFormat: "Raw",
+      safeSearch: "Off",
+    },
+    headers: {
+      "X-BingApis-SDK": "true",
+      "X-RapidAPI-Key": "740d6c8fcamsh537b1e793f27e8ep18db1ejsnbf58664a5522",
+      "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
+    },
+  };
+
+  const cryptoNews = await axios(options).then(({ data }) => {
+    return data;
+  });
+  res.json({ status: "ok", cryptoNews });
+});
+
+
 
 app.listen(port, () => {
   console.log(`Server has started on port ${port}`)
